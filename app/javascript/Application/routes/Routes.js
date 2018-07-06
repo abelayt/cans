592ea350ee93bda155e697ae2fad850bedaee730
jon walker <jonathan.b.walker0@gmail.com>
2018-07-06 15:21:45 -0700

    update route

--- app/javascript/Application/routes/Routes.js
+++ app/javascript/Application/routes/Routes.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { AssessmentContainer, Client, ClientsContainer } from '../components';
-import { ChildForm } from '../components/ChildForm';
+import { ClientForm } from '../components/Client';
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/clients/:childId/assessments/:id?" component={AssessmentContainer} />
-        <Route exact path="/clients/new" component={ChildForm} />
+        <Route exact path="/clients/new" component={ClientForm} />
         <Route path="/clients/:id" component={Client} />
         <Route exact path="/" component={ClientsContainer} />
       </Switch>
